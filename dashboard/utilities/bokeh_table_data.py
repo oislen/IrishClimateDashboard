619e0c0ba73012714031bbe4c92a13528dbb933f
Oisin <oisin.leonard@gmail.com>
2026-02-26 21:43:18 +0000

    Aggregating table data using existing line data function

--- dashboard/utilities/bokeh_table_data.py
+++ dashboard/utilities/bokeh_table_data.py
@@ -1,14 +1,22 @@
+from beartype import beartype
 import polars as pl
 from bokeh.models import ColumnDataSource, TableColumn
+from utilities.bokeh_line_data import bokeh_line_data
 
-def bokeh_table_data(master_data):
+@beartype
+def bokeh_table_data(
+    master_data:pl.DataFrame,
+    stat:str,
+    agg_level:str,
+    counties:list):
     """
     """
-    # filter for desired statistic and the previous full calendar year
-    max_datetime = master_data.select(pl.col("date").max().dt.strftime("%Y").str.to_datetime("%Y") - pl.duration(days=1)).to_series()[0]
-    data = master_data.filter((pl.col("date") <= max_datetime))
-    columns = master_data.columns
-    dataSource = ColumnDataSource(data.to_dict(as_series=False))
+    # run master data through line data function to aggregate up to desired level
+    bokeh_line_data_dict = bokeh_line_data(master_data=master_data, stat=stat, agg_level=agg_level, counties=counties)
+    agg_data = bokeh_line_data_dict['agg_data']
+    # possibly re-use or re-purpose bokeh line data function
+    columns = agg_data.columns
+    dataSource = ColumnDataSource(agg_data.to_dict(as_series=False))
     dataColumns = [TableColumn(field=col, title=col, width=15+int(len(col)*5)) for col in columns]
     bokeh_table_data_dict = {"dataSource":dataSource, "dataColumns":dataColumns}
     return bokeh_table_data_dict
