7e2a97dc6a8170a2189da8c10847e1894f1f95d4
Oisin <oisin.leonard@gmail.com>
2026-02-26 21:58:16 +0000

    Added some additional formating of the table values

--- dashboard/utilities/bokeh_table_data.py
+++ dashboard/utilities/bokeh_table_data.py
@@ -1,5 +1,6 @@
 from beartype import beartype
 import polars as pl
+import polars.selectors as cs
 from bokeh.models import ColumnDataSource, TableColumn
 from utilities.bokeh_line_data import bokeh_line_data
 
@@ -13,10 +14,10 @@ def bokeh_table_data(
     """
     # run master data through line data function to aggregate up to desired level
     bokeh_line_data_dict = bokeh_line_data(master_data=master_data, stat=stat, agg_level=agg_level, counties=counties)
-    agg_data = bokeh_line_data_dict['agg_data']
+    agg_data = bokeh_line_data_dict['agg_data'].drop(["date", "index"]).rename({"date_str":agg_level}).with_columns(cs.numeric().round(2))
     # possibly re-use or re-purpose bokeh line data function
     columns = agg_data.columns
     dataSource = ColumnDataSource(agg_data.to_dict(as_series=False))
-    dataColumns = [TableColumn(field=col, title=col, width=15+int(len(col)*5)) for col in columns]
+    dataColumns = [TableColumn(field=col, title=col, width=15+int(len(col)*10)) for col in columns]
     bokeh_table_data_dict = {"dataSource":dataSource, "dataColumns":dataColumns}
     return bokeh_table_data_dict
