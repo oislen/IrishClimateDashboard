c0b2856b7535fa7fac8267075dff48223520c74e
Oisin <oisin.leonard@gmail.com>
2026-03-01 14:07:11 +0000

    Added separate defaults for table width and table height

--- dashboard/table/bokeh_table_plot.py
+++ dashboard/table/bokeh_table_plot.py
@@ -3,6 +3,9 @@ import cons
 from bokeh.models import DataTable, Div
 from bokeh.layouts import column
 
+table_height = cons.FIG_SETTING['height']
+table_width=820
+
 @beartype
 def bokeh_table_plot(bokeh_data_dict:dict):
     """
@@ -10,13 +13,13 @@ def bokeh_table_plot(bokeh_data_dict:dict):
     data_table = DataTable(
         source=bokeh_data_dict['dataSource'],
         columns=bokeh_data_dict['dataColumns'],
-        width=int(cons.FIG_SETTING['width']*1.25),
-        height=cons.FIG_SETTING['height'],
+        width=table_width,
+        height=table_height,
         #stylesheets=[stylesheet],
         autosize_mode="none",
         #index_header=" ",
     )
     # add count of rows
-    n_rows = Div(text=f"<b>Total Rows: {len(bokeh_data_dict['dataSource'].data[bokeh_data_dict['dataColumns'][0].field])}</b>", width=cons.FIG_SETTING['width'], height=25)
+    n_rows = Div(text=f"<b>Total Rows: {len(bokeh_data_dict['dataSource'].data[bokeh_data_dict['dataColumns'][0].field])}</b>", width=table_width, height=25)
     data_table = column(data_table, n_rows)
     return data_table
