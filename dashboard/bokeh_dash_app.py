696b8c6414780d1a6663ec989a3171bbe2aa3ba6
Oisin <oisin.leonard@gmail.com>
2026-03-03 23:06:35 +0000

    Added main function

--- dashboard/bokeh_dash_app.py
+++ dashboard/bokeh_dash_app.py
@@ -8,24 +8,34 @@ from line.bokeh_line_dash import bokeh_line_dash
 from map.bokeh_map_dash import bokeh_map_dash
 from table.bokeh_table_dash import bokeh_table_dash
 
+def main():
+    """Generates the bokeh dashboard
+
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    bokeh.layouts.Tabs
+        The interactive bokeh line dashboard
+    """
+    # initialise each dashboard
+    dashboard_line = bokeh_line_dash()
+    dashboard_map = bokeh_map_dash()
+    dashboard_table = bokeh_table_dash()
+    # construct tab panels
+    panel_line_tab = TabPanel(child=dashboard_line, title='Time Series', name="panel_line_tab")
+    panel_map_tab = TabPanel(child=dashboard_map, title='GIS Map', name="panel_map_tab")
+    panel_table_tab = TabPanel(child=dashboard_table, title='Data Table', name="panel_table_tab")
+    # return combined tabs
+    return Tabs(tabs=[panel_line_tab, panel_map_tab, panel_table_tab], name="layout")
+
 # set up logging
 lgr = logging.getLogger()
 lgr.setLevel(logging.INFO)
-
-# initialise and structure bokeh line dashboard
-dashboard_line = bokeh_line_dash()
-panel_line_tab = TabPanel(child=dashboard_line, title='Time Series')
-
-# initialise and structure bokeh map dashboard
-dashboard_map = bokeh_map_dash()
-panel_map_tab = TabPanel(child=dashboard_map, title='GIS Map')
-
-# initialise and structure bokeh map dashboard
-dashboard_table = bokeh_table_dash()
-panel_table_tab = TabPanel(child=dashboard_table, title='Data Table')
-
-# create combined dashboard
-layout = Tabs(tabs=[panel_line_tab, panel_map_tab, panel_table_tab])
-
+# clear the document to remove old references
+curdoc().clear()
+# create dashboard
+layout = main()
 # deploy bokeh server and add dashboard layout
 curdoc().add_root(layout)
