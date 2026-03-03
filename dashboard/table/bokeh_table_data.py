47531d66629f547b88fc39df707ef6a6dfc357eb
Oisin <oisin.leonard@gmail.com>
2026-03-03 14:52:17 +0000

    Added logic to apply polars filter expression to filter data

--- dashboard/table/bokeh_table_data.py
+++ dashboard/table/bokeh_table_data.py
@@ -10,7 +10,9 @@ def bokeh_table_data(
     master_data:pl.DataFrame,
     stat:str,
     agg_level:str,
-    counties:list):
+    counties:list,
+    filter_expression_list:list=[]
+    ):
     """
     """
     # run master data through line data function to aggregate up to desired level
@@ -18,7 +20,9 @@ def bokeh_table_data(
     # calculate reference file for range slider min max values
     min_max_ref_dict = bokeh_line_data_dict['agg_data'].select(cons.col_options).describe().filter(pl.col("statistic").is_in(["min","max"])).to_dict(as_series=False)
     agg_data = bokeh_line_data_dict['agg_data'].drop(["date", "index"]).rename({"date_str":agg_level}).with_columns(cs.numeric().round(2))
-    # TODO: Add measure filter logic here
+    # add measure filter logic here
+    if filter_expression_list != []:
+        agg_data = agg_data.filter(pl.all_horizontal((filter_expression_list)))
     # pass polars filter expression through function parameters and apply filter here post aggregation
     columns = agg_data.columns
     dataSource = ColumnDataSource(agg_data.to_dict(as_series=False))
